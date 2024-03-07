eaf0ce4295c75e7a8f6ca6f5e869e62c126bed0d
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-07 10:51:24 -0500

    added array of questions for user to answer about shape and colors of logo

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateSvg = require("./utils/generateSvg")
 
-//to do: create promtp for text wiuth up to 3 letters, text color, shape, shepe color
-
+//Below is my array of questions for the user about the shape and colors they would like
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Please enter the text you would like displayed inside your logo! Text can be up to 3 letters.",
+        name: "text",
+    },
+    {
+        type: "input",
+        message: "Please enter the color keyword(red, white, blue) or hexadecimal number(#8152bf) you would like the color of the text to be.",
+        name: "textColor",
+    },
+    {
+        type: "input",
+        message: "Please select the desired shape for your logo from the choices.",
+        name: "shape",
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: "input",
+        message: "Please enter the color keyword(red, white, blue) or hexadecimal number(#8152bf) you would like the color of the shape to be.",
+        name: "shapeColor",
+    },
+])
 
 //to do: create a function to make the svg file
 
