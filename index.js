c7da13148caaa345d440f01461c2025900f4528f
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-07 10:34:58 -0500

    added consts in index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//to do: create consts
-
+//Below are all the consts necessary for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateSvg = require("./utils/generateSvg")
 
 //to do: create promtp for text wiuth up to 3 letters, text color, shape, shepe color
 
