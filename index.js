82a78c0be0fab86eab952be9681ffb1f0760b7f8
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-07 10:28:45 -0500

    added folders, added to do comments

---
+++ index.js
@@ -0,0 +1,13 @@
+//to do: create consts
+
+
+//to do: create promtp for text wiuth up to 3 letters, text color, shape, shepe color
+
+
+//to do: create a function to make the svg file
+
+
+//to do: create function to inialize app
+
+
+//to do: call the function to initalize
---
+++ utils/generateSvg.js
@@ -0,0 +1,10 @@
+//to do: set up parent shape
+
+
+//to do: set up classes for extending shape as circle, triangle, and square
+
+
+//to do: generate svg for new shapes with correct colors
+
+
+//to do: export shape to new folder
