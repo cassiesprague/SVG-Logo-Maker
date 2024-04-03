bc5109495e980191e61c7a0b3e98932fafb1879a
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-04-03 14:19:18 -0400

    fixed gitignore and added svg class conductor in index.js to set the text and shape elements'

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+//Used the following link to help with making this application
+//https://www.google.com/search?sca_esv=c0e14f80e7366f47&sca_upv=1&sxsrf=ACQVn0-rkeJimByVFhc3aBy2_f0aZxTYRQ:1712165948168&q=how+to+make+a+svg+logo+maker&tbm=vid&source=lnms&prmd=visnbmtz&sa=X&ved=2ahUKEwigjIyZy6aFAxXag4kEHTtVDIYQ0pQJegQICxAB&biw=934&bih=565&dpr=1.5#fpstate=ive&vld=cid:7d5aee99,vid:GJYMcLus3v0,st:0
 //Below are all the consts necessary for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateSvg = require("./utils/generateSvg")
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
-//Below is my array of questions for the user about the shape and colors they would like inside of the function to create the logo after questions have been answered
-function 
-inquirer.prompt([
+//Below conducts the svg class which renders the methods for setting shape and text
+class Svg{
+    constructor(){
+        this.textElement = ""
+        this.shapeElement = ""
+    }
+    render(){
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text, color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
+
+
+//Below is my array of questions for the user about the shape and colors they would like
+const questions = [
     {
         type: "input",
         message: "Please enter the text you would like displayed inside your logo! Text can be up to 3 letters.",
@@ -27,8 +46,7 @@ inquirer.prompt([
         message: "Please enter the color keyword(red, white, blue) or hexadecimal number(#8152bf) you would like the color of the shape to be.",
         name: "shapeColor",
     },
-])
-
+]
 
 //to do: create function to inialize app
 
